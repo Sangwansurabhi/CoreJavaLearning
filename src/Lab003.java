af554d1e934de374fc24ddd9b4dac9646fa522df
Sangwansurabhi <sangwansurabhi9876@gmail.com>
2023-12-21 20:51:28 +0530

    Hello world

--- src/Lab003.java
+++ src/Lab003.java
@@ -1,2 +1,7 @@
-package src;public class Lab003 {
+package src;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        System.out.println("Third codeee for git");
+    }
 }
--- src/Lab005.java
+++ src/Lab005.java
@@ -1,7 +1,7 @@
 package src;
 
-public class Lab004 {
+public class Lab005 {
     public static void main(String[] args) {
-
+        System.out.println("Hello world");
     }
 }
