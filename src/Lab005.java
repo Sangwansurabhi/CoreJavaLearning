5c8d340a16720a332d7471371c1e53df97cd5b67
Sangwansurabhi <sangwansurabhi9876@gmail.com>
2023-12-21 21:41:19 +0530

    6th Commit

--- src/Lab005.java
+++ src/Lab005.java
@@ -2,6 +2,6 @@ package src;
 
 public class Lab005 {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world......");
     }
 }
